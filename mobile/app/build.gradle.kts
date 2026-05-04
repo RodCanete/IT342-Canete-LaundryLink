525a55cc0f92498753ed0de80e499cd80b6b19a9
Rod Gabrielle Cañete <98006488+RodCanete@users.noreply.github.com>
2026-05-04 13:03:01 +0800

    chore(mobile): add browser and test dependencies

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -55,25 +55,20 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-
-    // Navigation Component (XML-based)
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-
-    // Retrofit + OkHttp
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    // Jetpack DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+    // Chrome Custom Tabs for PayMongo checkout
+    implementation("androidx.browser:browser:1.8.0")
 
-    // Testing (keep these as-is)
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
